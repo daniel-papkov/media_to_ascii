6cd94e6f8d525383a03248b84f5a69b48a34d1f1
swish-fushi <62179259+swish-fushi@users.noreply.github.com>
2022-04-27 20:58:39 +0300

    first version(24x24)
    first version can covert small pictures into ascii code(24x24)

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace png_to_asci
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tab_pic = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_path = new System.Windows.Forms.TextBox();
+            this.btn_convert = new System.Windows.Forms.Button();
+            this.pbx_preview = new System.Windows.Forms.PictureBox();
+            this.tabControl1.SuspendLayout();
+            this.tab_pic.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_preview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tab_pic);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1361, 607);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tab_pic
+            // 
+            this.tab_pic.Controls.Add(this.pbx_preview);
+            this.tab_pic.Controls.Add(this.btn_convert);
+            this.tab_pic.Controls.Add(this.txt_path);
+            this.tab_pic.Controls.Add(this.button1);
+            this.tab_pic.Location = new System.Drawing.Point(4, 22);
+            this.tab_pic.Name = "tab_pic";
+            this.tab_pic.Padding = new System.Windows.Forms.Padding(3);
+            this.tab_pic.Size = new System.Drawing.Size(1353, 581);
+            this.tab_pic.TabIndex = 0;
+            this.tab_pic.Text = "png_to_ascii";
+            this.tab_pic.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(122, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "browse image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_path
+            // 
+            this.txt_path.Location = new System.Drawing.Point(122, 164);
+            this.txt_path.Name = "txt_path";
+            this.txt_path.Size = new System.Drawing.Size(311, 20);
+            this.txt_path.TabIndex = 1;
+            this.txt_path.Visible = false;
+            // 
+            // btn_convert
+            // 
+            this.btn_convert.Location = new System.Drawing.Point(1065, 94);
+            this.btn_convert.Name = "btn_convert";
+            this.btn_convert.Size = new System.Drawing.Size(131, 64);
+            this.btn_convert.TabIndex = 2;
+            this.btn_convert.Text = "convert";
+            this.btn_convert.UseVisualStyleBackColor = true;
+            this.btn_convert.Click += new System.EventHandler(this.btn_convert_Click);
+            // 
+            // pbx_preview
+            // 
+            this.pbx_preview.Location = new System.Drawing.Point(439, 25);
+            this.pbx_preview.Name = "pbx_preview";
+            this.pbx_preview.Size = new System.Drawing.Size(606, 526);
+            this.pbx_preview.TabIndex = 3;
+            this.pbx_preview.TabStop = false;
+            this.pbx_preview.Click += new System.EventHandler(this.pbx_preview_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1364, 608);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tab_pic.ResumeLayout(false);
+            this.tab_pic.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_preview)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tab_pic;
+        private System.Windows.Forms.Button btn_convert;
+        private System.Windows.Forms.TextBox txt_path;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pbx_preview;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+
+namespace png_to_asci
+{
+    public partial class Form1 : Form
+    {
+        int cmd_width = 236;//tested and that is the size for my pc
+        int cmd_height = 63;//tested and that is the size for my pc
+                            //3.74 ratio in cmd
+                            //2.32 ratio in text vertical
+        public  int width, height;
+        Image source;//we will add the picture to here so we wont lose resolution
+        string gray_scale = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,^`'.                                                   ";//detailed stolen from google
+        //in case of console this is white to black
+        //(depends on background color and negetives so in html this is backwards by default)
+
+        string short_gray_scale = " .:-=+*#%@";
+        //same but shorter
+        public Form1()
+        {
+            InitializeComponent();
+            //SetConsoleFont
+            //for (int i = 0; i < 300; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            //fdlg.Title = "C# Corner Open File Dialog";
+            fdlg.InitialDirectory = @"c:\";
+            fdlg.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
+            fdlg.FilterIndex = 2;
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                txt_path.Text = fdlg.FileName;
+                txt_path.Visible = true;
+                pbx_preview_Click(sender, e);
+            }
+        }
+
+        private void pbx_preview_Click(object sender, EventArgs e)
+        {
+            Image img = Image.FromFile(txt_path.Text); ;
+            pbx_preview.Image = img;
+            source = img;
+            pbx_preview.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        public static Bitmap ResizeImage(Image image, int new_width, int new_height)
+        {
+            var destRect = new Rectangle(0, 0, new_width, new_height);
+            var destImage = new Bitmap(new_width, new_height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }           
+            return destImage;
+        }
+
+        private void btn_convert_Click(object sender, EventArgs e)
+        {
+            var new_bitmap=MakeGrayscale(new Bitmap(txt_path.Text));
+            pbx_preview.Image = new_bitmap;
+            //new_bitmap = ResizeImage(new_bitmap, width * 2, height);
+            width *= 2;
+            char[,] arr=gray_scale_to_ascii(new_bitmap);
+            for (int x = 0; x < arr.GetLength(0); x++)
+            {
+                for (int y = 0; y < arr.GetLength(1); y++)
+                {
+                    Console.Write(arr[y,x]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static Bitmap MakeGrayscale(Bitmap original)
+        {
+            //create a blank bitmap the same size as original
+            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+
+            //get a graphics object from the new image
+            Graphics g = Graphics.FromImage(newBitmap);
+
+            //create the grayscale ColorMatrix
+            ColorMatrix colorMatrix = new ColorMatrix(
+               new float[][]
+               {
+                 new float[] {.3f, .3f, .3f, 0, 0},
+                 new float[] {.59f, .59f, .59f, 0, 0},
+                 new float[] {.11f, .11f, .11f, 0, 0},
+                 new float[] {0, 0, 0, 1, 0},
+                 new float[] {0, 0, 0, 0, 1}
+               });
+
+            //create some image attributes
+            ImageAttributes attributes = new ImageAttributes();
+
+            //set the color matrix attribute
+            attributes.SetColorMatrix(colorMatrix);
+
+            //draw the original image on the new image
+            //using the grayscale color matrix
+            g.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height),
+               0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
+
+            //dispose the Graphics object
+            g.Dispose();
+            return newBitmap;
+        }
+
+        //returns a string with \n's with ascii chars
+        private char[,] gray_scale_to_ascii(Bitmap copy)
+        {
+            //final ratio is width of chars / width of image
+            // height of chars / height of image
+            //font Courier
+            char[,] output = new char[copy.Height, copy.Width];
+            for (int x = 0; x < copy.Height; x++)
+            {
+                for (int y = 0; y < copy.Width; y++)
+                {                 
+                    if(x==0)
+                    {
+                        output[x, y] = '~';
+                        continue;
+                    }
+                    Color originalColor = copy.GetPixel(x, y);                    
+                    output[x, y] = (gray_scale[(int)(originalColor.R/4)]);
+                }
+            }
+            return output;
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
